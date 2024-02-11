ffb2ccbffaef44303d306a06aad7b98fc3fb2c6c
Michael Tugendhaft <michael.t@sealsecurity.io>
2024-02-12 00:20:12 +0200

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Storybook only for now.
-    </h1>
-  )
-}
+    <h1 className="text-3xl font-bold underline">Storybook only for now.</h1>
+  );
+};
